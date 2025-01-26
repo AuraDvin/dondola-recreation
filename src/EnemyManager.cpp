8fddac7e05617a423d65456ca443b7d7f2cdc9a6
AuraDvin <edvin.garibovic@gmail.com>
2025-01-26 22:01:03 +0100

    basic functionality

--- src/EnemyManager.cpp
+++ src/EnemyManager.cpp
@@ -1,33 +1,58 @@
 #include "EnemyManager.h"
 
 
-EnemyManager::EnemyManager() {
+EnemyManager::EnemyManager(sf::RenderWindow &rw, Player &plyr) {
     list = new LinkedList<Enemy>;
+    Enemy::init(plyr, rw);
+    baseEnemy = new Enemy(0, "assets/square.png");
 }
 
 EnemyManager::~EnemyManager() {
     delete list;
+    delete baseEnemy;
 }
+
 //TODO:
 // * Render
 // * Update
 
 void EnemyManager::update(const float dt) {
     for (accumulatedTime += dt; accumulatedTime >= spawnPeriod; accumulatedTime -= spawnPeriod) {
-        // Spawn enemy
+        spawnEnemy();
     }
+
+    for (Node<Enemy> *i = list->getStart(); i != nullptr;) {
+        if (i->val.isOutOfBounds()) {
+            // id is now legal
+            notAllowedIds.erase(i->val.getID());
+            Node<Enemy> *tmp = i->next;
+            list->remove(i->val);
+            i = tmp;
+            continue;
+        }
+        i = i->next;
+    }
+
     // iterate and update all enemies
-    // for (Node<Enemy> &node: list) {
-    //     node.val.update(dt);
-    //     if (node.val.isOutOfBounds()) {
-    //
-    //     }
-    // }
+    list->update(dt);
 }
 
 void EnemyManager::render(sf::RenderWindow &window) {
     // iterate and draw all enemies
-    // for (auto &node: list) {
-    //     node.val.render(&window);
-    // }
+    list->render(window);
+}
+
+void EnemyManager::spawnEnemy() {
+    // make enemy object and give it the id
+    uint32_t id = getID();
+    list->add(Enemy(id, *baseEnemy));
+}
+
+uint32_t EnemyManager::getID() {
+    uint32_t id = rand();
+    while (notAllowedIds.find(id) != notAllowedIds.end()) {
+        id = rand();
+    }
+    notAllowedIds.insert(id);
+    return id;
 }
--- src/EnemyManager.h
+++ src/EnemyManager.h
@@ -1,6 +1,8 @@
 #ifndef ENEMYMANAGER_H
 #define ENEMYMANAGER_H
 
+#include <unordered_set>
+
 #include "Enemy.h"
 #include "LinkedList.h"
 
@@ -8,16 +10,22 @@
 class EnemyManager {
     float spawnPeriod = 2.f;
     float accumulatedTime = 0.f;
+    std::unordered_set<uint32_t> notAllowedIds;
     LinkedList<Enemy> *list;
+    Enemy *baseEnemy;
 
 public:
-    EnemyManager();
+    EnemyManager(sf::RenderWindow &rw, Player &plyr);
 
     ~EnemyManager();
 
     void update(float dt);
 
     void render(sf::RenderWindow &window);
+
+    void spawnEnemy();
+
+    uint32_t getID();
 };
 
 
