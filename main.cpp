7facf1c2eb68e266824b2519a637abfe9f042be3
AuraDvin <edvin.garibovic@gmail.com>
2025-09-09 10:38:44 +0200

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include <cstring>
-
 #include "src/GameManager.h"
 #include "src/MyDebugger.h"
-// #include "src/jsonReader.h"
 
 int main(const int argc, const char **argv) {
-    srand(static_cast<unsigned>(time(nullptr)));
     if (argc > 1) {
         if (strcmp(argv[1], "--debug") == 0) {
             debugger::debug_mode = true;
             debugger::print_debug("Debug mode enabled");
         }
     }
-    // Json reader demo
-    // const jsonReader jr("assets/dondola_test.json");
-    // std::cout << jr.json["frames"]["Sprite-0002 0."]["frame"];
     GameManager gm;
     return 0;
 }
--- src/prg.h
+++ src/prg.h
@@ -2,9 +2,9 @@
 #define PRG_H
 #include <random>
 
-// Will be used to obtain a seed for the random number engine
+/// Will be used to obtain a seed for the random number engine
 inline std::random_device rd;
-// Standard mersenne_twister_engine seeded with rd()
+/// Standard mersenne_twister_engine seeded with rd()
 inline std::mt19937 gen(rd());
 
 
