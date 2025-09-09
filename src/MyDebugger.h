91210027133e89655bc835afb8a042e076c2700a
AuraDvin <edvin.garibovic@gmail.com>
2025-09-09 10:18:10 +0200

    tried updating debugger
    tried, because I wanted to make the impl function private but didn't really work, so now it just has some extra documentation

--- src/MyDebugger.h
+++ src/MyDebugger.h
@@ -7,27 +7,32 @@
 
 #include <iostream>
 
-namespace debugger{
-    inline bool debug_mode = false;
+namespace debugger {
+    inline bool debug_mode = true;
 
-
-    inline void print_debug_impl() {}
-
-    template<typename T, typename... Args>
-    void print_debug_impl(T first, Args... args) {
-        if (debug_mode) {
-            std::cout << first;
+    namespace {
+        void print_debug_impl() {
         }
-        print_debug_impl(args...);
+
+        /// Private function for implementation of the print_debug function
+        /// Prints out first, then recursively calls itself until it runs out of arguments
+        /// Please call debugger::print_debug(args) function for debugging
+        template<typename T, typename... Args>
+        void print_debug_impl(T first, Args... args) {
+            std::cout << first << " ";
+            print_debug_impl(args...);
+        }
     }
 
-
+    /// Prints debug statements depending on launch arguments, if no --debug is present at program launch, no data will be printed.
+    /// @param args things to be printed.
     template<typename... Args>
     void print_debug(Args... args) {
-        print_debug_impl(args...);
-        if (debug_mode) {
-            std::cout << std::endl;
+        if (!debug_mode) {
+            return;
         }
+        print_debug_impl(args...);
+        std::cout << std::endl;
     }
 }
 
